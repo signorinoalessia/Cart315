941ec0b3d1a3caa3886ac94234cd68aa6d3f1487
Alessia Signorino <a_sign@cda122ev5635x.local>
2018-01-11 22:36:38 -0500

    Added shooting and shooting effects to arrow

---
+++ Alessia_Pastiche Game/Assets/Scripts/ArrowMove.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMove : MonoBehaviour {
+
+//	public float speed;
+//	public float JumpForce;
+//	public bool OnGround = false;
+
+	[Header("Attributes")]
+	public float range = 15f;
+	public float fireRate = 1f;
+	private float fireCountdown = 0f;
+	public float RotateSpeed = 30f;
+
+	public GameObject bubblePrefab;
+	public Transform firePoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.RightArrow) == true) {
+
+			//Debug.Log ("Right Arrow is pressed");
+			transform.Rotate(Vector3.up * RotateSpeed * Time.deltaTime);
+
+		}
+		if (Input.GetKey (KeyCode.LeftArrow) == true) {
+
+			//Debug.Log ("Right Arrow is pressed");
+			//UnityEngine.Transform.Rotate(0,-0.5,0);
+			transform.Rotate(-Vector3.up * RotateSpeed * Time.deltaTime);
+
+		}
+
+		if (fireCountdown <= 0f) {
+		
+			Shoot ();
+			fireCountdown = 1f / fireRate;
+		
+		}
+
+		fireCountdown -= Time.deltaTime;
+
+	}
+
+	void Shoot() {
+	
+		Debug.Log ("Shoot!");
+		GameObject bubbleGO = (GameObject)Instantiate (bubblePrefab, firePoint.position, firePoint.rotation);
+		Bubble bubble = bubbleGO.GetComponent<Bubble> ();
+
+		if (bubble != null)
+			bubble.Seek (target);
+
+	}
+
+	void OnCollisionEnter(Collision collision){
+
+		Debug.Log ("Collision!");
+		OnGround = true;
+
+	}
+	void OnCollisionExit(Collision collision){
+
+
+		OnGround = false;
+
+	}
+}
---
+++ Alessia_Pastiche Game/Assets/Scripts/Bubble.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour {
+
+	private Transform target;
+
+	public float speed = 70f;
+	public GameObject impactEffect;
+
+	public void Seek(Transform _target) {
+	
+		target = _target;
+	
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (target == null) {
+		
+			Destroy (gameObject);
+			return;
+		
+		}
+
+		Vector3 dir = target.position - transform.position;
+		float distanceThisFrame = speed * Time.deltaTime;
+
+		if (dir.magnitude <= distanceThisFrame) {
+			
+			HitTarget ();
+			return;
+
+		}
+
+		transform.Translate (dir.normalized * distanceThisFrame, Space.World);
+
+	}
+
+	void HitTarget() {
+
+		Debug.Log ("We hit something!");
+		GameObject effectIns = (GameObject)Instantiate (impactEffect, transform.position, transform.rotation);
+		Destroy (effectIns, 2f);
+		Destroy (gameObject);
+	}
+}
