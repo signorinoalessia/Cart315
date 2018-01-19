186bd9903db5bc6ca2b5323575d2a980306586a0
Alessia Signorino <a_sign@cda120ev5635x.cdaad.concordia.ca>
2018-01-18 20:17:04 -0500

    Attempt to make balls stick to each other

--- Bubble/Assets/MoveForward.cs
+++ Bubble/Assets/MoveForward.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveForward : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -11,8 +11,15 @@ public class MoveForward : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 		transform.Translate (new Vector3 (0, 1, 0));
 		//transform.position = transform.position + new Vector3 (0, 1, 0);
+	}
+
+	void OnCollisionEnter (Collision col) {
+	
+		if (col.gameObject.name == "Walls") {
+			transform.Translate (new Vector3 (0, -1, 0));
+		}
 	}
 }
