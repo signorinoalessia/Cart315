e267a7b3275d4c672998bf128f034c000a4e8e87
Alessia Signorino <a_sign@cda135ev5635w.cdaad.concordia.ca>
2018-01-24 09:24:22 -0500

    Added ball colors

--- Bubble/Assets/BallBehaviour.cs
+++ Bubble/Assets/BallBehaviour.cs
@@ -7,6 +7,8 @@ public class BallBehaviour : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
+		GetComponent<Renderer> ().material.color = new Color (Random.value,Random.value,Random.value,Random.value);
+
 	}
 	
 	// Update is called once per frame
@@ -19,9 +21,10 @@ public class BallBehaviour : MonoBehaviour {
 		Debug.Log ("Ball has collided with" + col.transform.tag);
 		//collider.rigidbody.velocity = new Vector3 (0, 0, 0);
 
-		if(col.transform.tag == "wall") {
+		if((col.transform.tag == "wall")||(col.transform.tag == "ball")) {
 
-			collider.transform.GetComponent<Rigidbody>().velocity = new Vector3 (0, 0, 0);
+			//collider.transform.GetComponent<Rigidbody>().velocity = new Vector3 (0, 0, 0);
+			GetComponent<Rigidbody> ().isKinematic = true;
 
 		}
 
