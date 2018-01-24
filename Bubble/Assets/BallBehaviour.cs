1016357ffebaed88b69590d429cc6220d1b00e0e
Alessia Signorino <a_sign@cda135ev5635w.cdaad.concordia.ca>
2018-01-24 09:05:30 -0500

    collsion with wall

---
+++ Bubble/Assets/BallBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+}
+
+	void OnCollisionEnter(Collision col) {
+		
+		Debug.Log ("Ball has collided with" + col.transform.tag);
+		//collider.rigidbody.velocity = new Vector3 (0, 0, 0);
+
+		if(col.transform.tag == "wall") {
+
+			collider.transform.GetComponent<Rigidbody>().velocity = new Vector3 (0, 0, 0);
+
+		}
+
+	}
+	
+}
