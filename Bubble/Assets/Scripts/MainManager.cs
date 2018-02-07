89298cc6f8270ab57f9193cbe517a687f9939eeb
Alessia Signorino <a_sign@cda120ev5635x.cdaad.concordia.ca>
2018-02-07 12:33:23 -0500

    Timer now works

--- Bubble/Assets/Scripts/MainManager.cs
+++ Bubble/Assets/Scripts/MainManager.cs
@@ -6,29 +6,32 @@ using UnityEngine.UI;
 public class MainManager : MonoBehaviour {
 
 	public int TotalScore;
+	public int Timeleft;
 
 	// Use this for initialization
 	void Start () {
 
-//		StartCoroutine (CountDown ());
+		StartCoroutine (CountDown ());
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-//		IEnumerator CountDown() {
-//			
-//		while (Timeleft > 0) {
-//		
-//			yield return new WaitForSeconds (1);
-//				Timeleft--;
-//				//if (Timeleft=-5)
-//		
-//		}
-//		}
+
 
 		GameObject.Find("ScoreLabel").GetComponent<Text>().text = "Score: "+ TotalScore;
 
 	}
+
+	IEnumerator CountDown() {
+
+		while (Timeleft > 0) {
+
+			yield return new WaitForSeconds (1);
+			Timeleft--;
+			//if (Timeleft=-5)
+		}
+	}
+
 }
