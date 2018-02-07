cacc51660bdd528cc919ceab8b90ad463d83134f
Alessia Signorino <a_sign@cda120ev5635x.cdaad.concordia.ca>
2018-02-07 09:32:46 -0500

    Tracks score and Labels it

---
+++ Bubble/Assets/Scripts/MainManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainManager : MonoBehaviour {
+
+	public int TotalScore;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GameObject.Find("ScoreLabel").GetComponent<Text>().text = "Score: "+ TotalScore;
+
+	}
+}
