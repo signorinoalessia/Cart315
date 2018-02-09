b9270312803be9239ad0ecd9023668815580f3fd
Alessia Signorino <a_sign@cda133ev5635x.local>
2018-02-08 21:06:04 -0500

    Added cave shape

--- Bubble/Assets/Scripts/MoveForward.cs
+++ Bubble/Assets/Scripts/MoveForward.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveForward : MonoBehaviour {
-	
+
+	public int Speed;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,14 +14,14 @@ public class MoveForward : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		transform.Translate (new Vector3 (0, 1, 0));
+		transform.Translate (new Vector3 (1, 0, 0));
 		//transform.position = transform.position + new Vector3 (0, 1, 0);
 	}
 
 	void OnCollisionEnter (Collision col) {
 	
-		if (col.gameObject.name == "Walls") {
-			transform.Translate (new Vector3 (0, -1, 0));
+		if (col.gameObject.name == "Wall") {
+			transform.Translate (new Vector3 (-1, 0, 0));
 
 		}
 	}
