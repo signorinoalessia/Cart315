fee99abab37ad9b0505dc2393de1ba9aa39f7204
Alessia Signorino <a_sign@cda047ev5715x.local>
2018-02-10 15:14:12 -0500

    Moving rock now works!!

--- Bubble/Assets/Scripts/MoveForward.cs
+++ Bubble/Assets/Scripts/MoveForward.cs
@@ -1,28 +1,45 @@
-﻿using System.Collections;
+﻿// Reference: TurnTheGameOn https://www.youtube.com/watch?v=AfwmRYQRsbg
+// Vector3.Lerp: https://docs.unity3d.com/ScriptReference/Vector3.Lerp.html
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveForward : MonoBehaviour {
 
 	public int Speed;
+	public Transform movingRock;
+	public Transform position1;
+	public Transform position2;
+	public Vector3 newPosition; 
+	public string currentState;
+	public float smooth;
+	public float resetTime;
 
 	// Use this for initialization
 	void Start () {
-		
+		ChangeTarget ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	// Update is called once per frame (constantly)
+	void FixedUpdate () {
 		
-		transform.Translate (new Vector3 (1, 0, 0));
-		//transform.position = transform.position + new Vector3 (0, 1, 0);
+		movingRock.position = Vector3.Lerp (movingRock.position, newPosition, smooth * Time.deltaTime);
 	}
 
-	void OnCollisionEnter (Collision col) {
-	
-		if (col.gameObject.name == "Wall") {
-			transform.Translate (new Vector3 (-1, 0, 0));
-
+	void ChangeTarget() {
+		if(currentState == "Moving to Position 1") {
+			currentState = "Moving to Position 2";
+			newPosition = position2.position;
 		}
+		else if(currentState == "Moving to Position 2") {
+			currentState = "Moving to Position 1";
+			newPosition = position1.position;
+		}
+		else if(currentState== ""){
+			currentState = "Moving to Position 2";
+			newPosition = position2.position;
+		}
+		Invoke ("ChangeTarget", resetTime);
 	}
 }
